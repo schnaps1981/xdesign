3a575228f2031b0d01361bb377194ec4d3ede191
schnaps1981 <schnaps1981@gmail.com>
2020-03-10 13:50:29 +0700

    продолжил 10.03.2020 - 13:50

--- app/src/main/java/com/y/xdesign/ui/adapter/PhotoViewHolder.java
+++ app/src/main/java/com/y/xdesign/ui/adapter/PhotoViewHolder.java
@@ -1,17 +1,11 @@
 package com.y.xdesign.ui.adapter;
 
-import android.graphics.drawable.Drawable;
 import android.view.View;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
-import com.bumptech.glide.load.DataSource;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
-import com.bumptech.glide.load.engine.GlideException;
-import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.target.Target;
 import com.y.xdesign.R;
 import com.y.xdesign.app.GlideApp;
@@ -34,24 +28,14 @@ public class PhotoViewHolder extends BaseViewHolder<PhotoModel> {
     @Override
     public void bind(PhotoModel model) {
 
-
+        ivPhoto.layout(0,0,0,0);
 
         GlideApp
                 .with(itemView.getContext())
                 .load(model.getPrew())
                 .diskCacheStrategy(DiskCacheStrategy.ALL)
-                .listener(new RequestListener<Drawable>() {
-                    @Override
-                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
-                        return false;
-                    }
-
-                    @Override
-                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
-                        return false;
-                    }
-                })
                 .error(R.drawable.ic_error_outline_black_24dp)
+                .override(Target.SIZE_ORIGINAL)
                 .into(ivPhoto);
     }
 }
